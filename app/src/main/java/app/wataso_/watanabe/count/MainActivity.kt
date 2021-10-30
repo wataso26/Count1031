7dd990e3f08548dd19bde28f7af7e75ee8fb5ec2
lifeistech <2wata2wata2@gmail.com>
2021-10-31 00:56:02 +0900

    "作った"

--- app/src/main/java/app/wataso_/watanabe/count/MainActivity.kt
+++ app/src/main/java/app/wataso_/watanabe/count/MainActivity.kt
@@ -2,10 +2,23 @@ package app.wataso_.watanabe.count
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text :TextView = findViewById(R.id.text)
+        val button : Button = findViewById(R.id.button)
+
+        var count :Int = 0
+
+        button.setOnClickListener {
+            count =count +1
+            text.text = count.toString()
+        }
     }
+
 }
